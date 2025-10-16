34d581eee295c8c1f838dc84274d857fecc08519
rishi <rishi@gmail.com>
2025-10-16 14:43:24 +0530

    modified file

--- crud.py
+++ crud.py
@@ -18,26 +18,31 @@ def create_user(user: User):
     user_count += 1
     return user
 
+@app.get("/users")
 def get_all_users():
     if not user_details:
         raise HTTPException(status_code=404, detail="No users found")  
     else:
         return user_details
-    
+
+@app.post("/create")    
 def create_new_user(user: User):
     return create_user(user)
 
+@app.get("/user/{user_id}")
 def get_single_user(user_id: int):
     if not user_details:
         raise HTTPException(status_code=404, detail="No users found")  
     return user_details.get(user_id, "User not found")
 
+@app.put("/user/{user_id}")
 def update_user(user_id: int, user: User):
     if user_id not in user_details:
         raise HTTPException(status_code=404, detail="User not found")
     user_details[user_id] = user
     return user
 
+@app.delete("/user/{user_id}")  
 def delete_user(user_id: int):
     if user_id not in user_details:
         raise HTTPException(status_code=404, detail="User not found")
